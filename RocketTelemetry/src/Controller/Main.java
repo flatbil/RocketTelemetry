d8f9b8f3cb9585c39c9dd4816bec5d9cfaeb7cdb
William Almond <walmond@gmail.com>
2016-07-20 15:57:40 -0700

    Added the mysql connector to lib.

--- RocketTelemetry/src/Controller/Main.java
+++ RocketTelemetry/src/Controller/Main.java
@@ -4,5 +4,15 @@ package Controller;
  * @version Summer 2016
  */
 public class Main {
+	// JDBC driver name and database URL
+	   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	   static final String DB_URL = "jdbc:mysql://localhost:3306/Telemetry";
 
+	   //  Database credentials
+	   static final String USER = "root";
+	   static final String PASS = "Winston1";
+	   
+	   public static void main(String[] args){
+		   
+	   }
 }
